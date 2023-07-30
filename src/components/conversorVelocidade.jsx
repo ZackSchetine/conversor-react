7bf262de3b9b7ecb7c4951cd9549d8220d4e92dc
eduardosaatkamp <eduardosaatkamp@gmail.com>
2023-07-30 01:21:38 -0300

    Adicionado o evento de converter ao clicar

--- src/components/conversorVelocidade.jsx
+++ src/components/conversorVelocidade.jsx
@@ -6,25 +6,41 @@ const itemList = ['km/h para m/s', 'm/s para km/h'];
 export default function ConvVelocidade() {
     const num = 10.8;
     const value = 3.6;
-    let result = 0;
-
     const [selectedOption, setSelectedOption] = React.useState(null);
-
-    console.log(selectedOption);
+    const [inputValue, setInputValue] = React.useState('');
 
     const handleOptionSelect = (option) => {
         setSelectedOption(option);
     };
 
+    // mexer aqui
+    const handleConversion = () => {
+        switch (selectedOption) {
+            case itemList[0]:
+                const kmhParaMs = parseFloat(inputValue) / value;
+                setInputValue(kmhParaMs.toFixed(2)); // Exibir o resultado com 2 casas decimais
+                break;
+            case itemList[1]:
+                const msParaKmh = parseFloat(inputValue) * value;
+                setInputValue(msParaKmh.toFixed(2)); // Exibir o resultado com 2 casas decimais
+                break;
+            default:
+                setInputValue('');
+        }
+    };
+
     const renderSelectedComponent = () => {
         switch (selectedOption) {
             case itemList[0]:
-                result = num * value;
                 return (
                     <div>
                         <h3> km/h -- m/s </h3>
-                        <input placeholder='Informe o valor a ser convertido'></input>
-                        <button type="button">Converter</button>
+                        <input
+                            placeholder='Informe o valor'
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                        <button type="button" onClick={handleConversion}>Converter</button>
                         <span style={{
                             display: 'inline-block',
                             border: '1px solid #ccc',
@@ -34,16 +50,30 @@ export default function ConvVelocidade() {
                             backgroundColor: '#f9f9f9',
                             color: '#333',
                             cursor: 'default'
-                        }}>{result}</span>
-                    </div >
+                        }}>{inputValue}</span>
+                    </div>
                 );
             case itemList[1]:
-                result = num / value;
                 return (
                     <div>
                         <h3> m/s -- km/h </h3>
                         <br />
-                        <span>{result}</span>
+                        <input
+                            placeholder='Informe o valor'
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
+                        <button type="button" onClick={handleConversion}>Converter</button>
+                        <span style={{
+                            display: 'inline-block',
+                            border: '1px solid #ccc',
+                            marginTop: '30px',
+                            padding: '5px',
+                            borderRadius: '4px',
+                            backgroundColor: '#f9f9f9',
+                            color: '#333',
+                            cursor: 'default'
+                        }}>{inputValue}</span>
                     </div>
                 );
             default:
