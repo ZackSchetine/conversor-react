12fd55e534dd3f58cb4fcac522da013bdf645a7b
eduardosaatkamp <eduardosaatkamp@gmail.com>
2023-07-30 01:58:41 -0300

    Adicionado conversão de temperatura c para kelvin e o inverso

--- src/components/conversorTemperatura.jsx
+++ src/components/conversorTemperatura.jsx
@@ -1,71 +1,81 @@
 import * as React from 'react';
 import DropDown from './dropDown';
 
-const itemList = ['Celsius para Fahrenheit', 'Celsius para Kelvin'];
+const itemList = [
+  'Celsius para Fahrenheit',
+  'Fahrenheit para Celsius',
+  '',
+  'Celsius para Kelvin',
+  'Kelvin para Celsius',
+];
 
 export default function ConvTemperatura() {
-    const num = 20;
-    const [selectedOption, setSelectedOption] = React.useState(null);
-    const [inputValue, setInputValue] = React.useState('');
-    const [conversionResult, setConversionResult] = React.useState('');
+  const num = 20;
+  const [selectedOption, setSelectedOption] = React.useState(itemList[0]); // Iniciar com a primeira opção selecionada
+  const [inputValue, setInputValue] = React.useState('');
+  const [conversionResult, setConversionResult] = React.useState('');
 
-    const handleOptionSelect = (option) => {
-        setSelectedOption(option);
-        setInputValue('');
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setInputValue('');
+    setConversionResult('');
+  };
+
+  const handleConversion = () => {
+    switch (selectedOption) {
+      case itemList[0]:
+        const celsiusToFahrenheit = (parseFloat(inputValue) * 9 / 5) + 32;
+        setConversionResult(celsiusToFahrenheit.toFixed(2));
+        break;
+      case itemList[1]:
+        const fahrenheitToCelsius = (parseFloat(inputValue) - 32) * 5 / 9;
+        setConversionResult(fahrenheitToCelsius.toFixed(2));
+        break;
+      case itemList[2]:
+        const celsiusToKelvin = parseFloat(inputValue) + 273.15;
+        setConversionResult(celsiusToKelvin.toFixed(2));
+        break;
+      case itemList[3]:
+        const kelvinToCelsius = parseFloat(inputValue) - 273.15;
+        setConversionResult(kelvinToCelsius.toFixed(2));
+        break;
+      default:
         setConversionResult('');
-    };
+    }
+  };
 
-    const handleConversion = () => {
-        switch (selectedOption) {
-            case itemList[0]:
-                const celsiusToFahrenheit = (parseFloat(inputValue) * 9 / 5) + 32;
-                setConversionResult(celsiusToFahrenheit.toFixed(2));
-                break;
-            case itemList[1]:
-                const celsiusToKelvin = parseFloat(inputValue) + 273.15;
-                setConversionResult(celsiusToKelvin.toFixed(2));
-                break;
-            default:
-                setConversionResult('');
-        }
-    };
-
-    const renderSelectedComponent = () => {
-        return (
-            <div>
-                {selectedOption && (
-                    <>
-                        <h3>{selectedOption}</h3>
-                        <input
-                            placeholder='Informe o valor em Celsius'
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)}
-                        />
-                        <button type="button" onClick={handleConversion}>Converter</button>
-                        {conversionResult && (
-                            <span style={{
-                                display: 'inline-block',
-                                border: '1px solid #ccc',
-                                marginTop: '30px',
-                                padding: '5px',
-                                borderRadius: '4px',
-                                backgroundColor: '#f9f9f9',
-                                color: '#333',
-                                cursor: 'default'
-                            }}>{conversionResult}</span>
-                        )}
-                    </>
-                )}
-            </div>
-        );
-    };
-
+  const renderSelectedComponent = () => {
     return (
-        <div>
-            <h2>Menu de conversão de temperatura</h2>
-            <p>Selecione uma das opções abaixo:</p>
-            <DropDown itens={itemList} onSelect={handleOptionSelect} />
-            {renderSelectedComponent()}
-        </div>
+      <div>
+        <h3>{selectedOption}</h3>
+        <input
+          placeholder='Informe o valor'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="button" onClick={handleConversion}>Converter</button>
+        {conversionResult && (
+          <span style={{
+            display: 'inline-block',
+            border: '1px solid #ccc',
+            marginTop: '30px',
+            padding: '5px',
+            borderRadius: '4px',
+            backgroundColor: '#f9f9f9',
+            color: '#333',
+            cursor: 'default'
+          }}>{conversionResult}</span>
+        )}
+      </div>
     );
+  };
+
+  return (
+    <div>
+      <h2>Menu de conversão de temperatura</h2>
+      <p>Selecione uma das opções abaixo:</p>
+      <DropDown itens={itemList} onSelect={handleOptionSelect} />
+      {renderSelectedComponent()}
+    </div>
+  );
 }
