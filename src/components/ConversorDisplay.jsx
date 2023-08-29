3ce3c804e1fc1abafa35b7e33f02d6b7a5d10eff
Zack <zackschetine@gmail.com>
2023-08-29 18:03:07 -0300

    Removendo os componentes duplicados

--- src/components/ConversorDisplay.jsx
+++ src/components/ConversorDisplay.jsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import './ConvVelocidade.css';
+import './convVelocidade.css';
 
 export default function Display(props) {
     return (
-        <div>
+        <div className='teste'>
             <h3>{props.selectedOption}</h3>
             <input
                 placeholder='Informe o valor'
                 value={props.inputValue}
                 onChange={(e) => props.setInputValue(e.target.value)}
             />
-            <button type="button" onClick={props.handleConversion}>Converter</button>
+            <button className='converter-button' type="button" onClick={props.handleConversion}>Converter</button>
             <br />
             {props.conversionResult && (
                 <span className='custom-styled-span'>{props.conversionResult}</span>
